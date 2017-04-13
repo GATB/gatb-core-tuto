88a87eb464a2926aacacb9657a50f48d9a46e925
Patrick Durand <patrick.durand@inria.fr>
2017-04-13 14:45:37 +0200

    add snippets

--- snippets/io2.cpp
+++ snippets/io2.cpp
@@ -1,26 +1,20 @@
 #include <gatb/gatb_core.hpp>
 
 int main (int argc, char* argv[]){
+  
   /* we expect a sequence file name on the cmd-line*/
   if(argc != 2){
     printf("%s filename\n",argv[0]);
     exit(1);
   }
-
+  
   /* we open that file to read sequences from */
   IBank * inbank = Bank::open(argv[1]);
-  /* we open a second file to write sequences.
-   * Using BankFasta means we create a fasta file
-   */
-  IBank*  outBank = new BankFasta ("outbank");
 
-  /* we iterate over sequences from inout file */
-  Iterator<Sequence>* it = inbank->iterator();
-  for (it->first(); !it->isDone(); it->next()){
-    /* we write the sequence into the new bank */
-    Sequence& seq = it->item();
-    outBank->insert (seq);
-  }
-  /* do not forget to flush content */
-  outBank->flush();
+  /* we get bank information */
+  int64_t   nbseq = inbank ->estimateNbItems ();
+  u_int64_t totalsize =   inbank ->estimateSequencesSize ();
+  std::cout  << "# sequences: " << nbseq     << std::endl;
+  std::cout  << "# letters: "   << totalsize << std::endl;
+
 }
