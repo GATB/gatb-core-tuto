cec7ae3cfaf471ce4b4c5cfbc6db12846e894f54
Patrick Durand <patrick.durand@inria.fr>
2017-04-14 15:04:59 +0200

    add notice: add your code here

--- snippets/io3.cpp
+++ snippets/io3.cpp
@@ -1,26 +1,16 @@
 #include <gatb/gatb_core.hpp>
 
 int main (int argc, char* argv[]){
+  
   /* we expect a sequence file name on the cmd-line*/
   if(argc != 2){
     printf("%s filename\n",argv[0]);
     exit(1);
   }
-
+  
   /* we open that file to read sequences from */
   IBank * inbank = Bank::open(argv[1]);
-  /* we open a second file to write sequences.
-   * Using BankFasta means we create a fasta file
-   */
-  IBank*  outBank = new BankFasta ("outbank");
 
-  /* we iterate over sequences from inout file */
-  Iterator<Sequence>* it = inbank->iterator();
-  for (it->first(); !it->isDone(); it->next()){
-    /* we write the sequence into the new bank */
-    Sequence& seq = it->item();
-    outBank->insert (seq);
-  }
-  /* do not forget to flush content */
-  outBank->flush();
+  /* we get bank information */
+  /*** ADD YOUR CODE HERE ***/  
 }
--- snippets/io4.cpp
+++ snippets/io4.cpp
@@ -1,28 +1,26 @@
 #include <gatb/gatb_core.hpp>
 
 int main (int argc, char* argv[]){
-  
   /* we expect a sequence file name on the cmd-line*/
   if(argc != 2){
     printf("%s filename\n",argv[0]);
     exit(1);
   }
-  
+
   /* we open that file to read sequences from */
   IBank * inbank = Bank::open(argv[1]);
+  /* we open a second file to write sequences.
+   * Using BankFasta means we create a fasta file
+   */
+  IBank*  outBank = new BankFasta ("outbank");
 
   /* we iterate over sequences from inout file */
   Iterator<Sequence>* it = inbank->iterator();
   for (it->first(); !it->isDone(); it->next()){
-    /* we display some sequence data on standard output (console) */
+    /* we write the sequence into the new bank */
     Sequence& seq = it->item();
-    /* sequence size and comment (i.e. header) */
-    std::cout << "[" << seq.getDataSize() << "] " << seq.getComment()  << std::endl;
-    /* sequence itself (i.e. nucleotides) */
-    std::cout << seq.toString() << std::endl;
-
-    /*** ADD YOUR CODE HERE ***/
-
+    outBank->insert (seq);
   }
-
+  /* do not forget to flush content */
+  outBank->flush();
 }
